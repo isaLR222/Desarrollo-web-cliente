6b910fa65b4baf3d948c99b49c3ade805ce02889
isaLR222 <isabellodeiroramos@gmail.com>
2025-09-30 10:56:11 +0200

    ejer8

---
+++ LodeiroMariaUnidad3/Ejercicio8/Ejercicio8.js
@@ -0,0 +1,19 @@
+/*Haz un programa que pida al usuario cadenas del tipo: 
+“nombre:apellidos:teléfono:email:codigopostal” y que te muestre: 
+◦ El código postal. 
+◦ Los apellidos 
+◦ El email. 
+◦ Suponiendo un formato de email “direccion@servidor.com” te muestre el servidor asociado (la parte a la derecha del @).
+◦ El teléfono.
+*/
+function ejecuta(){
+    let nombre=document.getElementById("nombre");
+    let apellidos=document.getElementById("apellidos");
+    let telefono=document.getElementById("telefono");
+    let email=document.getElementById("email");
+    let postal=document.getElementById("postal");
+    let ser=email.substr("@");
+    document.getElementById("resultado").innerHTML="andwwd";
+   
+
+}
