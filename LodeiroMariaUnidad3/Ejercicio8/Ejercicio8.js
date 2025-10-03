90dedd1f48d38a286c125efb412697f5c8cc680e
isaLR222 <isabellodeiroramos@gmail.com>
2025-10-03 11:40:59 +0200

    8

--- LodeiroMariaUnidad3/Ejercicio8/Ejercicio8.js
+++ LodeiroMariaUnidad3/Ejercicio8/Ejercicio8.js
@@ -7,13 +7,11 @@
 ◦ El teléfono.
 */
 function ejecuta(){
-    let nombre=document.getElementById("nombre");
-    let apellidos=document.getElementById("apellidos");
-    let telefono=document.getElementById("telefono");
-    let email=document.getElementById("email");
-    let postal=document.getElementById("postal");
-    let ser=email.substr("@");
-    document.getElementById("resultado").innerHTML="andwwd";
-   
-
+    let nombre=document.getElementById("nombre").value;
+    let apellidos=document.getElementById("apellidos").value;
+    let telefono=document.getElementById("telefono").value;
+    let email=document.getElementById("email").value;
+    let postal=document.getElementById("postal").value;
+    let ser=email.substr("@").value;
+    document.getElementById("resultado").innerHTML=`Nombre: ${nombre} <br> Apellidos: ${apellidos} <br> Email: ${email} <br> Servidor: ${ser} <br> Telefono: ${telefono} <br> Postal: ${postal} `;
 }
