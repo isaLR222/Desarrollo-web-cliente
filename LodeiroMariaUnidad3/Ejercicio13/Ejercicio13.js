e34a2270e22f286b455af63a9a0106fabadf85f7
isaLR222 <isabellodeiroramos@gmail.com>
2025-10-06 13:07:42 +0200

    fkjag ab

--- LodeiroMariaUnidad3/Ejercicio13/Ejercicio13.js
+++ LodeiroMariaUnidad3/Ejercicio13/Ejercicio13.js
@@ -1,8 +1,19 @@
 /*Crea una página web que tenga 3 botones (<button>), crear, parar y borrar. El botón crear mostrar una ventana de 500*200 y que se mueva automáticamente de 10 px hacia la derecha y hacia abajo cada 100 milisegundos, cuando llegue a la esquina inferior derecha deberá rebotar hacia la esquina superior izquierda. El botón parar detendrá la ventana y el cerrar la eliminará.*/
 var ventana;
 var timer=setInterval(()=>{
+    let x = ventana.screenX;
+    let y = ventana.screenY;
+    
     ventana.moveBy(10,10);
+    /*
+    if(x===1404&&y===759){
+        do{ventana.moveBy(-10,-10);}while(x===!0&&y===!0);
+    }*/
+    
+
 },100);
+
+
 
 function Crear(){
      ventana=window.open("","MiVentana","width=500,height=200");
@@ -13,8 +24,6 @@ ventana.document.write("Yupiiiiii");
 function Parar(){
     clearInterval(timer);
 }
-
-
 
 function Borrar(){
     ventana.close();
---
+++ LodeiroMariaUnidad3/Ejercicio14/Ejercicio14.js
@@ -0,0 +1,10 @@
+/*Realiza una web en la que el usuario pueda introducir el nombre de un ordenador con el siguiente formato:
+DOCXXXT
+025PXXXT
+XXX sera un numero entre el 000 y el 252
+T puede ser A, B  o C
+Se le mostrará al usuario la siguiente dirección IP dependiendo de el nombre y de la letra T
+T= A  → 10.42.68.XXX
+T= B   → 10.42.69.XXX
+T= C   → 10.52.178.XXX
+*/
