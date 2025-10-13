17c5bdf498c5096ccba3cfa500eeb04a876642d0
isaLR222 <isabellodeiroramos@gmail.com>
2025-10-13 11:58:32 +0200

    Ejer 1

--- LodeiroMariaUnidad4/Ejercicio1/Arrays.js
+++ LodeiroMariaUnidad4/Ejercicio1/Arrays.js
@@ -1,5 +1,5 @@
 export var paises=["Albania","España","Sri Lanka"];
-
+//Contar
 export function contar(){
     let cont=0;
     for(var i=0; i<paises.length;i++){
@@ -7,8 +7,36 @@ export function contar(){
     }
     return cont;
 }
-
-export function list(){
-    return 
+//Ordenar
+export function listOrdenada(){
+    return paises.sort();
 }
 
+export function listOrdenadaR(){
+    return paises.reverse();
+}
+//Añadir
+export function añadiF(pais){
+    paises.push(pais);
+}
+export function añadiP(pais){
+    paises.unshift(pais);
+}
+
+//Borrar
+export function eliminarP(){
+    paises.shift();
+}
+
+export function eliminarF(){
+    paises.pop();
+}
+
+//Consultar
+export function consultarPos(numero){
+    return paises[numero-1]
+}
+
+export function consultarNom(nombre){
+    return paises.indexOf(nombre)+1;
+}
--- LodeiroMariaUnidad4/Ejercicio1/Ejercicio1.js
+++ LodeiroMariaUnidad4/Ejercicio1/Ejercicio1.js
@@ -1,6 +1,49 @@
 import * as exportado from "./Arrays.js";
+
+//Contar elementos
 document.getElementById("cant").addEventListener("click",function(){
     alert(exportado.contar())});
 
-document.getElementById()
+//document.getElementById()
 
+//Ordenar elementos
+document.getElementById("list").addEventListener("click",function(){
+    let respuesta=prompt("1- ordenado, 2- al revés");
+    switch(respuesta){
+        case "1": alert(exportado.listOrdenada()); break;
+        case "2": alert(exportado.listOrdenadaR()); break;
+        default: alert("Numero introducido incorrecto"); break;
+    }
+})
+
+//Añadir elemento
+document.getElementById("añadir").addEventListener("click", function(){
+    let pais=prompt("Introduce un país a añadir");
+    let respuesta=prompt("1- Al final 2- Al principio");
+    switch(respuesta){
+        case "1": exportado.añadiF(pais); break;
+        case "2": exportado.añadiP(pais); break;
+        default: alert("Numero introducido incorrecto"); break;
+    }
+})
+
+//Borrar elemento
+document.getElementById("borrar").addEventListener("click", function(){
+    let respuesta=prompt("1- eliminar ultimo 2- eliminar primero");
+     switch(respuesta){
+        case "1": exportado.eliminarF(); break;
+        case "2": exportado.eliminarP(); break;
+        default: alert("Numero introducido incorrecto"); break;
+    }
+})
+
+//Consultar
+document.getElementById("consulta").addEventListener("click", function(){
+    let respuesta=prompt("1- Acceder por posicion 2- Acceder por nombre");
+     switch(respuesta){
+        case "1": let pos=parseInt(prompt("Posicion")); alert(exportado.consultarPos(pos)); break;
+        case "2": let nom=prompt("Nombre"); alert(exportado.consultarNom(nom)) ; break;
+        default: alert("Numero introducido incorrecto"); break;
+    }
+
+})
